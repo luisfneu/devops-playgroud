e99267073dfa47fe22ba8874fe2d453729560b0c
Luis Fernando Neu <luisneu@gmail.com>
2026-03-24 11:24:12 -0300

    var

--- jenkins-sonar/variables.tf
+++ jenkins-sonar/variables.tf
@@ -40,3 +40,41 @@ variable "storage_size" {
   type        = string
   default     = "5Gi"
 }
+
+# SonarQube variables
+variable "sonarqube_namespace" {
+  description = "Kubernetes namespace for SonarQube"
+  type        = string
+  default     = "sonarqube"
+}
+
+variable "sonarqube_chart_version" {
+  description = "SonarQube Helm chart version"
+  type        = string
+  default     = "10.3.0"
+}
+
+variable "sonarqube_admin_password" {
+  description = "SonarQube admin password"
+  type        = string
+  sensitive   = true
+  default     = "admin123"
+}
+
+variable "sonarqube_node_port" {
+  description = "NodePort for SonarQube service (30000-32767)"
+  type        = number
+  default     = 32090
+}
+
+variable "sonarqube_storage_size" {
+  description = "Persistent volume size for SonarQube data"
+  type        = string
+  default     = "5Gi"
+}
+
+variable "sonarqube_db_storage_size" {
+  description = "Persistent volume size for SonarQube PostgreSQL"
+  type        = string
+  default     = "5Gi"
+}
